4ac891f0afe4f8593a22625f3b0c3898ba8bebf5
Gigi <gigi.khomeriki@gmail.com>
2016-06-08 23:25:14 +0400

    route update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,43 @@
 |
 */
 
-Route::get('/', 'Controller@showIndex');
-Route::get('/dashboard', 'Controller@showDashboard');
-Route::get('/profile', 'Controller@showProfile');
-Route::get('/inst-profile', 'Controller@showInstProfile');
+Route::group(['middleware' => ['web']], function () {
+
+	Route::get('/', 'Controller@showIndex');
+	Route::get('/dashboard', 'Controller@showDashboard');
+	Route::get('/profile', 'Controller@showProfile');
+	Route::get('/my-courses', 'Controller@myCourses');
+	Route::get('/my-messages', 'Controller@myMessages');
+	Route::get('/blog-post', 'Controller@showBlogPost');
+	Route::get('/blog', 'Controller@showBlog');
+	Route::get('/contact', 'Controller@showContact');
+	Route::get('/course-forum-thread', 'Controller@showCourseForumThread');
+	Route::get('/course-forums', 'Controller@showCourseForums');
+	Route::get('/forum-category', 'Controller@showForumCategory');
+	Route::get('/course', 'Controller@showCourse');
+	Route::get('/forum-thread', 'Controller@showForumThread');
+	Route::get('/forum', 'Controller@showForum');
+	Route::get('/directory', 'Controller@showDirectory');
+	Route::get('/show-course/{course_id}', 'Controller@showShowCourse');
+
+
+	Route::get('lang/{lang}', ['as'=>'lang.switch', 'uses'=>'LanguageController@switchLang']);
+
+
+
+	Route::get('/sign-up', 'Controller@showSignUp');
+	Route::get('/main-login', 'Controller@showLogin');
+
+	Route::get('/edit-course', 'Controller@showEditCourse');
+	Route::get('/instructor-courses', 'Controller@showInstCourses');
+	Route::get('/instructor-profile', 'Controller@showInstProfile');
+	Route::get('/instructor-dashboard', 'Controller@showInstDashboard');
+	Route::get('/instructor-messages', 'Controller@showInstMessages');
+	Route::get('/instructor-statement', 'Controller@showInstStatement');
+
+
+});
+
 Route::get('login', 'Controller@getLogin');
 Route::get('register', 'Controller@getRegister');
 Route::post('register', 'Controller@postRegister');
